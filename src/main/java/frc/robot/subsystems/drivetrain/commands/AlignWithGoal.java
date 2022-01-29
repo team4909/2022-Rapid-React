f63cb4007c71d62e92356120f42f59028827bf2e
Nity6000 <31748962+Nity6000@users.noreply.github.com>
2022-01-29 12:04:13 -0500

    add stuff later

---
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/AlignWithGoal.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivetrain.DrivetrainSubsystem;
+
+public class AlignWithGoal extends CommandBase{
+
+    DrivetrainSubsystem drivetrain = DrivetrainSubsystem.getInstance();
+
+    public AlignWithGoal() {
+
+    }
+    
+    public void initialize() {
+        // TODO Auto-generated method stub
+        super.initialize();
+    }
+    
+
+
+}
