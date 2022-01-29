1dc30922b1e384839f66c8b91868100fc8bf1581
Team4909 <mpatankar06@gmail.com>
2022-01-28 20:59:13 -0500

    switched to PPSwerveCommand

--- src/main/java/frc/robot/subsystems/drivetrain/commands/TrajectoryFollow.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/TrajectoryFollow.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.drivetrain.commands;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -14,16 +15,16 @@ import frc.robot.subsystems.drivetrain.DrivetrainSubsystem;
 
 public class TrajectoryFollow extends CommandBase {
 
-    private String pathName;
-    
+    private String m_pathName;
+
 
     public TrajectoryFollow() {
         //TODO add default stationary path
-        this.pathName = "";
+        m_pathName = "";
     }
 
     public TrajectoryFollow(String pathName) {
-        this.pathName = pathName;
+        m_pathName = pathName;
     }
 
     @Override
@@ -32,22 +33,23 @@ public class TrajectoryFollow extends CommandBase {
 
         PathPlannerTrajectory trajectory = null;
         try {
-            trajectory = PathPlanner.loadPath(pathName, 4.9, 4);
+            trajectory = PathPlanner.loadPath(m_pathName, 4.9, 4);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        PathPlannerState finalAngle = ((PathPlannerState) trajectory.getEndState());
-        new SwerveControllerCommand(trajectory, 
-        DrivetrainSubsystem.getInstance()::getCurrentPose, 
+        ProfiledPIDController thetaController = new ProfiledPIDController(1.5, 0, 0, new TrapezoidProfile.Constraints(DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND, Math.pow(DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND, 2)));
+        thetaController.enableContinuousInput(-180, 180);
+
+        new PPSwerveControllerCommand(trajectory, 
+        DrivetrainSubsystem.getInstance()::getCurrentPose,
         DrivetrainSubsystem.getInstance().getKinematics(), 
         new PIDController(3, 0, 0), 
         new PIDController(3, 0, 0),
-        new ProfiledPIDController(1.5, 0, 0, new TrapezoidProfile.Constraints(DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND, Math.pow(DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND, 2))),
-        () -> finalAngle.holonomicRotation,
+        thetaController, 
         DrivetrainSubsystem.getInstance()::actuateModulesAuto, 
         DrivetrainSubsystem.getInstance()).andThen(() -> DrivetrainSubsystem.getInstance().drive(new ChassisSpeeds(0.0, 0.0, 0.0))).schedule();
-    }
+        }
 
     @Override
     public void end(boolean interrupted) {
@@ -55,3 +57,18 @@ public class TrajectoryFollow extends CommandBase {
     }
 
 }
+
+/* OLD PATHFOLLOWING COMMAND
+PathPlannerState finalAngle = ((PathPlannerState) trajectory.getEndState());
+
+        // new SwerveControllerCommand(trajectory, 
+        // DrivetrainSubsystem.getInstance()::getCurrentPose, 
+        // DrivetrainSubsystem.getInstance().getKinematics(), 
+        // new PIDController(3, 0, 0), 
+        // new PIDController(3, 0, 0),
+        // thetaController,
+        // () -> finalAngle.holonomicRotation,
+        // DrivetrainSubsystem.getInstance()::actuateModulesAuto, 
+        // DrivetrainSubsystem.getInstance()).andThen(() -> DrivetrainSubsystem.getInstance().drive(new ChassisSpeeds(0.0, 0.0, 0.0))).schedule();
+    
+ */
