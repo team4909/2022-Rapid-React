2b16f577e2cf0d47d1a7ece1e2612a08a19fdb58
Team4909 <mpatankar06@gmail.com>
2022-02-05 12:07:29 -0500

    work in progress climber logic

--- src/main/deploy/pathplanner/Climber.java
+++ src/main/deploy/pathplanner/Climber.java
@@ -7,6 +7,7 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
